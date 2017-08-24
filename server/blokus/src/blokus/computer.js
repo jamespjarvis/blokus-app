374e5302f1dfa6aa663375b710656b25d15b8228
James Jarvis <jamespjarvis@gmail.com>
2017-08-23 21:09:38 -0400

    asdsa

--- server/blokus/src/blokus/computer.js
+++ server/blokus/src/blokus/computer.js
@@ -218,7 +218,7 @@ function Computer(game) {
       const pieceIdScore = result.piece;
       const distanceScore = result.positions.reduce((total, curr) => (distanceToCenter(curr)), 0);
       const cornerDistanceScore = result.positions.reduce((total, curr) => (distanceFromCorner(curr, player.id, testBoard)), 0);
-      return Object.assign(result, { score: (freeCornerScore / ((cornerDistanceScore + pieceIdScore) - distanceScore)) });
+      return Object.assign(result, { score: ((freeCornerScore / cornerDistanceScore - distanceScore) * pieceIdScore) });
     });
 
     if (scoredResults.length) {
